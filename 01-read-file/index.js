848b2aafa17da711c03cbbdd83f99e222a3f0b8f
dm-mrtnvch <dmvshn@gmail.com>
2024-01-17 14:22:48 +0400

    feat: 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+
+const fileName = 'text.txt'
+const filePath = path.join(__dirname, fileName);
+const readStream = fs.createReadStream(filePath);
+
+readStream.pipe(process.stdout);
